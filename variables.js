03348b27ab32501e51a2e917a02642de708a3925
alanjkg <alanjkg@gmail.com>
2014-02-12 19:42:02 -0500

    solved circle question

--- variables.js
+++ variables.js
@@ -7,11 +7,11 @@
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 
-var childnum = 3
-var wife = "Amber Heard"
-var place = "Toronto"
-var jobtitle = "Software Developer"
-console.log("You will be a " + jobtitle +" " + "in" + " " + place + ", and married to" + " "+ wife + " " + "with" + " " + childnum + " " + "kids.")
+var childnum = 3;
+var wife = "Amber Heard";
+var place = "Toronto";
+var jobtitle = "Software Developer";
+console.log("You will be a " + jobtitle +" " + "in" + " " + place + ", and married to" + " "+ wife + " " + "with" + " " + childnum + " " + "kids.");
 // EXERCISE: The Age Calculator
 
 // Forgot how old someone is? Calculate it!
@@ -21,11 +21,11 @@ console.log("You will be a " + jobtitle +" " + "in" + " " + place + ", and marri
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
 
-var currentyear = 2014
-var birthyear = 1989
-var agev1 = currentyear - birthyear
-var agev2 = agev1-1
-console.log("They are either" + " " +agev2 + " " + "or" + " " + agev1)
+var currentyear = 2014;
+var birthyear = 1989;
+var agev1 = currentyear - birthyear;
+var agev2 = agev1-1;
+console.log("They are either" + " " +agev2 + " " + "or" + " " + agev1);
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -37,11 +37,11 @@ console.log("They are either" + " " +agev2 + " " + "or" + " " + agev1)
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
-var currentage = 24
-var maxage = 100
-var foodperday = 2
-var maximilian = ((maxage - currentage)*365)*foodperday
-console.log("You will need" +" "+maximilian+ " "+ "to last you until the ripe old age of" + " " + maxage )
+var currentage = 24;
+var maxage = 100;
+var foodperday = 2;
+var maximilian = ((maxage - currentage)*365)*foodperday;
+console.log("You will need" +" "+maximilian+ " "+ "to last you until the ripe old age of" + " " + maxage );
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
@@ -49,6 +49,11 @@ console.log("You will need" +" "+maximilian+ " "+ "to last you until the ripe ol
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
+
+var radius = 15;
+var circumference = 2 * Math.PI * radius;
+var area = Math.PI * radius * radius;
+console.log("the area is" + " " + area);
 
 // EXERCISE: The Temperature Converter
 
@@ -58,5 +63,8 @@ console.log("You will need" +" "+maximilian+ " "+ "to last you until the ripe ol
 // Convert it to fahrenheit and output "NN°C is NN°F".
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
+
+var tempc = 30
+var temp
 
 
